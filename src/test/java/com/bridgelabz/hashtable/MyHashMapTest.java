7408912b37e6f9980442c9e08aa7fbf31bac4417
Textspot <sachinkatarki1995@gmail.com>
2021-11-18 17:29:42 +0530

    Added:Find the frequency of words large paragraph paranoids of Hash Table

--- src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MyHashMapTest {
 	 private TestCase Assertions;
 
-	    //testCase to Find the Frequency of given words are true
+	 //testCase to Find the Frequency of given words are true
 	    @Test
 	    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
 	        String sentence = "To Be or not To be";
@@ -23,6 +23,26 @@ public class MyHashMapTest {
 	        int frequency = myHashMap.get("to");
 	        System.out.println(myHashMap);
 	        Assertions.assertEquals(2, frequency);
+	    }
+
+	    @Test
+	    public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+	        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+	                "because they keep putting themselves deliberately " +
+	                "into paranoid avoidable situations";
+	        MyHashMapParanoid<String, Integer> myLinkedHashMap = new MyHashMapParanoid<>();
+	        String[] words = sentence.toLowerCase().split(" ");
+	        for (String word : words) {
+	            Integer value = myLinkedHashMap.get(word);
+	            if (value == null)
+	                value = 1;
+	            else
+	                value += 1;
+	            myLinkedHashMap.add(word, value);
+	        }
+	        System.out.println(myLinkedHashMap);
+	        int frequency = myLinkedHashMap.get("paranoid");
+	        Assertions.assertEquals(3, frequency);
 	    }
 
 }
