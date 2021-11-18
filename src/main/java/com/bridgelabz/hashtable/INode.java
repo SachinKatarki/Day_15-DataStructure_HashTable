cf1474d9e0965b195d2c28c6225d0dc43a49797c
Textspot <sachinkatarki1995@gmail.com>
2021-11-18 16:57:36 +0530

    Added:Finding The Frequency table in Hash Table

--- src/main/java/com/bridgelabz/hashtable/INode.java
+++ src/main/java/com/bridgelabz/hashtable/INode.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.hashtable;
 
-public class INode{
+public class INode<K> {
+    K getKey();
+
+    void setKey(K key);
+
+    INode getNext();
+
+    void setNext(INode next);
     
 }
